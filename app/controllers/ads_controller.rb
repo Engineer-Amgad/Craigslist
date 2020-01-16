c166ebb11eacfbb8943213808ca193c0f1984c75
Engineer-Amgad <amgadmalik@yahoo.com>
2020-01-16 04:52:36 -0500

    created ads controller and ad index view.

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,3 +1,22 @@
 class AdsController < ApplicationController
-    
+    #CREATE
+        #New
+        #Make a get request to '/ads/new'
+
+    #READ
+        #Index
+        #Make a get request to '/ads'
+
+        get '/ads' do 
+            @ads = Ad.all
+            erb :'ads/index'
+        end 
+
+    #UPDATE
+        #Edit
+        #Make a get requrest to /'ads/:id/edit'
+
+    #DESTROY
+        #Make a delete rquest to '/ads/:id'
+
 end 
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,3 +1,3 @@
-class Ad < ActiveRecord::base
+class Ad < ActiveRecord::Base
     belongs_to :user
 end
