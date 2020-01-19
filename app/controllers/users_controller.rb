8551e3a9e013c6e8d4a5f62a9b4f10869cd7f644
Engineer-Amgad <amgadmalik@yahoo.com>
2020-01-19 09:34:47 -0500

    created sign up form for new users to sign up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
     
+    
+    #user can sign up
+    get '/signup' do
+        erb :'users/signup'
+    end 
+
+    post '/signup' do 
+        user = User.new(params)
+        if user.username.empty? || user.password.empty?
+            @error = "Username and password can not be blank."
+            erb :'user/signup'
+        elsif User.find_by(username: user.username)
+            @error ="An account with this username already exists."
+            erb :'user/signup'
+        else
+            user.save
+            session[user_id] = user.id
+            redirect '/ads'
+        end
+    end 
+    #user can delete account
+
 end 
